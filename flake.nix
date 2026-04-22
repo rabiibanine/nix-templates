3e4e96733836178e9e74f54ae675fbfbf1fc06a1
rabiibanine <rbanine@gmail.com>
2026-04-22 21:16:14 +0100

    Initial flake.nix

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+
+  description = "Nix templates";
+
+  inputs = {
+
+  };
+
+  outputs =
+    { self, ... }@inputs:
+    {
+      templates = {
+        python = {
+          path = "./templates/python";
+          description = "Python dev shell";
+        };
+        c = {
+          path = "./templates/c";
+          description = "The C language dev shell";
+        };
+        javascript = {
+          path = "./templates/javascript";
+          description = "Javascript dev shell";
+        };
+      };
+    };
+
+}

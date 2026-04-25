1ef855e86266fc40f55dea0b4ae288bdc9b0a7d3
rabiibanine <rbanine@gmail.com>
2026-04-25 11:00:39 +0100

    --amend

--- flake.nix
+++ flake.nix
@@ -10,22 +10,42 @@
     { self, ... }@inputs:
     {
       templates = {
+
         universal = {
           path = ./templates/universal;
           description = "Universal dev shell";
         };
+
         python = {
           path = ./templates/python;
           description = "Python dev shell";
         };
+
         c = {
           path = ./templates/c;
           description = "The C language dev shell";
         };
+
         javascript = {
           path = ./templates/javascript;
           description = "Javascript dev shell";
         };
+
+        java = {
+          path = ./templates/java;
+          description = "Java dev shell";
+        };
+
+        java-gradle = {
+          path = ./templates/java-gradle;
+          description = "Java-gradle dev shell";
+        };
+
+        java-maven = {
+          path = ./templates/java-maven;
+          description = "Java-maven dev shell";
+        };
+
       };
     };
 

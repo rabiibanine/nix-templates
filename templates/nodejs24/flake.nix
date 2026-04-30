5a364a75afe1703ea58a5102ecc86cb3a5ea1b7d
rabiibanine <rbanine@gmail.com>
2026-05-01 00:08:10 +0100

    feature: add nodejs24 template

---
+++ templates/nodejs24/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "NodeJS Dev Shell";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/b12141ef619e0a9c1c84dc8c684040326f27cdcc";
+    };
+  };
+
+  outputs =
+    { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          nodejs_24
+        ];
+
+        shellHook = ''
+          echo "NodeJS Dev Environment Loaded"
+        '';
+
+      };
+    };
+
+}
